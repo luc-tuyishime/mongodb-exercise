b110689eadf401e47fc924734c83a65833d32756
luc <luctunechi45@gmail.com>
2018-12-24 17:29:59 +0200

    function added

---
+++ solution.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("could not connect to MongoDB..", err))
+
+const courseSchema = new mongoose.Schema({
+  // once we have a schema, we compile to model down witch gives us a Class (Course)
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+})
+
+const Course = mongoose.model("Course", courseSchema)
+
+async function createCourse() {
+  const course = new Course({
+    // create object based on class(Course) and store in DB
+    name: "Angular Course",
+    author: "Luc",
+    tags: ["angular", "frontend dev"],
+    isPublished: true
+  })
+
+  const result = await course.save() // will save to the DB
+  console.log(result)
+}
+
+async function getCourses() {
+  const courses = await Course
+    .find({ isPublished: true })
+    .or([ { tags: 'frontend' }, { tags: 'backend' } ])
+    .sort('-price')
+    .select('name author price')
+  console.log(courses)
+}
+
+async function run(){
+  const courses = await getCourses()
+  console.log(courses)
+}
+
+run()
